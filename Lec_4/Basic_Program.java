cee6ca71b77b3b06433fedd2dc3f5c9a4aa842f2
KING <matadeenyadav202@gmail.com>
2026-03-31 15:02:37 +0530

    Create Basic_Program.java

---
+++ Lec_4/Basic_Program.java
@@ -0,0 +1,15 @@
+void main(){
+   System.out.println("Hello Duniya");
+   System.out.println("Hello Duniya"+"Hi Aman");
+   System.out.println(3);
+   System.out.println(3+2);
+   System.out.println(3*2);
+   System.out.println(3/2);
+   System.out.println(3%2);
+}
+
+// public class Lec_4 {
+//     static void main(String[] args) {
+//         System.out.println("Hello Aman");
+//     }
+// }
